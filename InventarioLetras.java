8fcf00e98de18640b393904007a9ce3338a01a78
maty0708 <matguka0@gmail.com>
2026-05-08 18:39:32 -0400

    Update InventarioLetras.java

--- InventarioLetras.java
+++ InventarioLetras.java
@@ -1,13 +1,29 @@
 public class InventarioLetras {
-
-    private int[] conteo;
+// atributos
+    private int [] conteo;
     private int totalCount;
-    private int nonZerocount
+    private int nonZerocount;
 
-    public InventarioLetras( String data){
+    // constructor
+    public InventarioLetras(String data){
         this.conteo = new int[26];
         this.totalCount = 0;
         this.nonZerocount = 0;
-    }
 
+        data = data.toLowerCase();
+
+        for(int i = 0; i < data.length();i++){
+            char c = data.charAt(i);
+
+            if (c >= 'a' && c <= 'z'){
+                int index = c - 'a';
+
+                if(this.conteo[index] == 0){
+                    this.nonZerocount++;
+                }
+                this.conteo[index]++;
+                this.totalCount++;
+            }
+        }
+    }
 }
