ed835348bd3a4d8f2f52b83384e3ffda8af4bf7a
maty0708 <matguka0@gmail.com>
2026-05-08 18:24:57 -0400

    Add files via upload

---
+++ InventarioLetras.java
@@ -0,0 +1,13 @@
+public class InventarioLetras {
+
+    private int[] conteo;
+    private int totalCount;
+    private int nonZerocount
+
+    public InventarioLetras( String data){
+        this.conteo = new int[26];
+        this.totalCount = 0;
+        this.nonZerocount = 0;
+    }
+
+}
