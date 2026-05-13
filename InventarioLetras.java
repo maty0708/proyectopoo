b3494b8fc9bedb3916a8d6f3a2c41ae5a1e88619
maty0708 <matguka0@gmail.com>
2026-05-13 18:26:45 -0400

    Update InventarioLetras.java

--- InventarioLetras.java
+++ InventarioLetras.java
@@ -26,4 +26,61 @@ public class InventarioLetras {
             }
         }
     }
+    //metodo 1
+    public int size(){
+        return this.totalCount;
+    }
+    // metodo 2
+    public boolean isEmpty(){
+        return this.nonZerocount == 0;
+    }
+// metodo 3
+    public int get(char letra){
+        letra = Character.toLowerCase(letra);
+
+        if (letra < 'a' || letra > 'z'){
+            throw new IllegalArgumentException("Letra invalida");
+        }
+        return this.conteo[letra - 'a'];
+    }
+    //metodo 4
+    public String toString(){
+        String resultado = "[";
+        for (int i = 0; i < 26; i++){
+            for (int j = 0; j < this.conteo[i]; j++){
+                resultado +=(char) (i + 'a');
+            }
+        }
+        resultado +="]";
+        return resultado;
+    }
+    // metodo 5
+    public char encriptarCesar(char letra){
+        if(!Character.isLetter(letra)){
+            return letra;
+        }
+       letra = Character.toLowerCase(letra);
+            return (char)(((letra - 'a' + 3) % 26) + 'a');
+
+    }
+    //metodo 6
+    public char desencriptarCesar(char letra){
+        if(!Character.isLetter(letra)){
+            return letra;
+        }
+
+        letra = Character.toLowerCase(letra);
+        return (char)(((letra - 'a' - 3 + 26) % 26) + 'a');
+    } //metodo 7
+    public String encriptarPalabra(String palabra){
+
+        String resultado = "";
+        for ( int i = 0; i < palabra.length(); i++){
+            char c = palabra.charAt(i);
+            resultado+=encriptarCesar(c);
+        }
+       return resultado;
+    }
 }
+
+
